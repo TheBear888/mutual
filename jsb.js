a26ccaf57a71abf1e99cba494cfc52049efe043f
Bear <91580311+TheBear888@users.noreply.github.com>
2022-05-10 15:17:28 +0800

    Update jsb.js

--- jsb.js
+++ jsb.js
@@ -212,8 +212,8 @@ let _0x134a17 = new Date(),
     _0x459e63 = 1.07,
     _0x2e716e = 0,
     _0x5bc515 = "ksjsb",
-    _0x180c0c = "https://raw.githubusercontent.com/TheBear888/mutual/main/1.json",
-    ksjsbjk = "https://raw.githubusercontent.com/TheBear888/mutual/main/2.json",
+    _0x180c0c = "https://raw-github.awyun.cc/TheBear888/mutual/main/2.json",
+    ksjsbjk = "https://raw-github.awyun.cc/TheBear888/mutual/main/2.json",
     _0x75eec0 = "https://127.0.0.1/";
 
 class _0x9d8dda {
